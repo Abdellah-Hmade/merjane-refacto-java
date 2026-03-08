da9d1f77c8fd092d6ea092eab6566c121186be58
abdellah-hmade <hmade.abdellah@gmail.com>
2026-03-08 18:28:14 +0000

    fix logic in seasonal products

--- api/src/main/java/com/nimbleways/springboilerplate/services/implementations/ProductService.java
+++ api/src/main/java/com/nimbleways/springboilerplate/services/implementations/ProductService.java
@@ -24,19 +24,23 @@ public class ProductService {
     }
 
     public void handleSeasonalProduct(Product p) {
-        if (LocalDate.now().plusDays(p.getLeadTime()).isAfter(p.getSeasonEndDate())) {
+        if ((LocalDate.now().isAfter(p.getSeasonStartDate()) && LocalDate.now().isBefore(p.getSeasonEndDate())
+                && p.getAvailable() > 0)) {
+            p.setAvailable(p.getAvailable() - 1);
+            productRepository.save(p);
+        }
+
+        else if (LocalDate.now().plusDays(p.getLeadTime()).isAfter(p.getSeasonEndDate())) {
             notificationService.sendOutOfStockNotification(p.getName());
             p.setAvailable(0);
             productRepository.save(p);
-        } else if (p.getSeasonStartDate().isAfter(LocalDate.now())) {
-            notificationService.sendOutOfStockNotification(p.getName());
-            productRepository.save(p);
-        } else {
+        }  else {
             notifyDelay(p.getLeadTime(), p);
         }
     }
 
     public void handleExpiredProduct(Product p) {
+
         if (p.getAvailable() > 0 && p.getExpiryDate().isAfter(LocalDate.now())) {
             p.setAvailable(p.getAvailable() - 1);
             productRepository.save(p);
